3508fb1ae26a42e727b76193a7f88e3a262fe2f0
henesy <henesy.dev@gmail.com>
2017-02-08 14:38:49 -0600

    making some boilerplate

--- port1/main.go
+++ port1/main.go
@@ -2,7 +2,26 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 )
+
+
+/* makes a handler to manage game urls */
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
+/* handles a game instance */
+
+/* handles the main screen */
+
 
 
 /* Portfolio One Go web game written in Golang by Sean Hinchee */
