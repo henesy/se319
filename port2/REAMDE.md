4ad5adb4dfc2a5dd6699ab68dc08fb6fe1f678a3
Sean Hinchee <Sean Hinchee>
2017-03-23 18:16:09 -0500

    port2 afternoon updates

--- port2/REAMDE.md
+++ port2/REAMDE.md
@@ -4,6 +4,10 @@ Node JS: https://nodejs.org/en/
 
 # To Run
 
+From the project's working directory:
+
 `node server.js`
 
+# To Use
 
+http://localhost:8080/
--- port2/server.js
+++ port2/server.js
@@ -6,8 +6,14 @@ var qs = require("querystring");
 var html = fs.readFileSync("./index.html");
 var welcome = fs.readFileSync("./welcome.html");
 var welcome_js = fs.readFileSync("./welcome_js.html");
+var paint = fs.readFileSync("./paint.html");
+var paint_js = fs.readFileSync("./paint_js.html");
 
 var version = "v0.0.1";
+
+// 100 max sessions
+var sessions = new Array(100);
+var s_num = 0;
 
 function getPath(request) {
   var path = url.parse(request.url).pathname;
@@ -46,17 +52,22 @@ http.createServer(function(req, res) {
 					var version = `;
 				js_string += version;
 
-				res.end("<html><h3>Page for: " + postData.name + "!</h3>" + welcome + js_string + welcome_js);
+				res.end("<html><h3>Username: " + postData.name + "!</h3>" + welcome + js_string + welcome_js);
 
 			} else if(postData.ordo == "paint") {
-                // painting page
-                res.writeHead(200, {"Content-Type": "text/html"});
-                var js_string = `
-                    <script>
-                    var session = `;
-                js_string += postData.session;
+                s_num++;
 
-                res.end("<html><h3>Page for session: " + postData.session + "!</h3>" + welcome + js_string + welcome_js);
+                // painting page
+                var sid = postData.session;
+
+                    res.writeHead(200, {"Content-Type": "text/html"});
+                    var js_string = `
+                        <script>
+                        var session = `;
+                    js_string += sid;
+
+                    res.end("<html><h3>Page for session: " + sid + "!</h3>" + paint + js_string + paint_js);
+
             } else {
 				res.writeHead(200, {"Content-Type": "text/plain"});
 				res.end("ERROR!");
