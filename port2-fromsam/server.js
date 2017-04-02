1cb8889188f023483f5c7349a2ac96db7b68cc58
Sean Hinchee <seh@iastate.edu>
2017-04-02 01:51:45 -0500

    port2 almost done, main features in

--- port2-fromsam/server.js
+++ port2-fromsam/server.js
@@ -12,6 +12,7 @@ var version = "v0.0.1";
 // 100 max sessions
 var sessions = new Array(100);
 var s_num = 0;
+var blankVal = sessions[0];
 
 function getPath(request) {
   var path = url.parse(request.url).pathname;
@@ -24,12 +25,20 @@ http.createServer(function(req, res) {
 	var reqData = "";
 
 	if(req.method === "GET") {
-		/* request for a page */
-		res.writeHead(200, {"Content-Type": "text/html"});
-		//response.writeHead(200, {"Content-Type": "text/plain"});
-		//response.write("Hello from the Node.js server!");
-		res.write(html);
-		res.end();
+		if(req.url.indexOf("pic") > -1) {
+			//getting image for a session
+			var img = fs.readFileSync('.' + req.url);
+			res.writeHead(200, {'Content-Type': 'image/png' });
+			res.end(img, 'binary');
+			console.log(req.url);
+		} else {
+			/* request for a page */
+			res.writeHead(200, {"Content-Type": "text/html"});
+			//response.writeHead(200, {"Content-Type": "text/plain"});
+			//response.write("Hello from the Node.js server!");
+			res.write(html);
+			res.end();
+		}
 	} else if(req.method === "POST") {
 		/* we're passing information for a page ;; use ordo to determine what page to load, maybe */
 		req.setEncoding("utf-8");
@@ -45,7 +54,7 @@ http.createServer(function(req, res) {
 			if(postData.ordo == "welcome") {
                 // welcome page
 				res.writeHead(200, {"Content-Type": "text/html"});
-				
+
 				//welcome = welcome.replace("$Version", version);
 
 				res.end("<html><h3>Username: " + postData.name + "!</h3>" + welcome);
@@ -55,12 +64,22 @@ http.createServer(function(req, res) {
 
                 // painting page
                 var sid = postData.session;
+				//var ans = postData.answer;
 
-                    res.writeHead(200, {"Content-Type": "text/html"});
-					paint = fs.readFileSync("./paint.html", "utf8"); //make live updates
-					paint = paint.replace("$Sid", sid);
-					
-                    res.end("<html><h3>Page for session: " + sid + "!</h3>" + paint);
+                res.writeHead(200, {"Content-Type": "text/html"});
+				paint = fs.readFileSync("./paint.html", "utf8"); //make live updates
+				paint = paint.replace("$Sid", sid);
+				paint = paint.replace("$PURL", "/pics/" + sid + ".png");
+				paint = paint.replace("$ANS", sessions[sid]);
+				if(sessions[sid] != blankVal) {
+					paint = paint.replace("$SAT", "$(\"#creator\").hide();");
+				} else {
+					//no answer yet
+					paint = paint.replace("$SAT", "$(\"#guessor\").hide();");
+				}
+
+
+                res.end("<html><h3>Page for session: " + sid + "!</h3>" + paint);
 
             } else if(postData.ordo == "picture") {
 				  var matches =  postData.canvas.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
@@ -69,6 +88,8 @@ http.createServer(function(req, res) {
 					if(err) {
 						return console.log(err);
 					}
+					sessions[postData.session] = postData.answer;
+					console.log(postData.answer + " for " + postData.session);
 
 					console.log("The file was saved!");
 				  });
